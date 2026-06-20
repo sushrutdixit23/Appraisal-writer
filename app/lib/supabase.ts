892afcc29f27f7a5fd8babb91aaf02628ddf0273
sushrutdixit23 <sushrut.dixit.23@gmail.com>
2026-06-20 14:54:01 +0530

    Fix supabase

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -8,6 +8,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
+    flowType: "pkce",
     storageKey: "zyntask-engage-session",
   },
 });
